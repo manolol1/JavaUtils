398ced353910f0b9e1506a05612ef5d37f98168d
manolol1 <manolol@mailbox.org>
2025-01-11 19:46:12 +0000

    Deploying to gh-pages from @ manolol1/JavaUtils@d7871e6bd061e60b9d2bdd437bdba5ce9446fa42 🚀

--- type-search-index.js
+++ type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"JavaUtils","l":"ArrayUtils"},{"p":"JavaUtils","l":"InputUtils"},{"p":"JavaUtils","l":"JavaUtils"},{"p":"JavaUtils","l":"MatrixUtils"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"JavaUtils","l":"ArrayUtils"},{"p":"JavaUtils","l":"InputUtils"},{"p":"JavaUtils","l":"JavaUtils"},{"p":"JavaUtils","l":"MatrixUtils"},{"p":"JavaUtils","l":"Timer"}];updateSearchResults();
